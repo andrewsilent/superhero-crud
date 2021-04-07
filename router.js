11e2fa4db432c8dd5d45e4e8ba50082a72debda4
andrewsilent <silent.netix@gmail.com>
2021-04-07 19:19:02 +0300

    add multer

--- router.js
+++ router.js
@@ -1,6 +1,16 @@
 const { Router } = require('express');
-
+const multer = require('multer');
 const router = Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}.${file.originalname}`);
+  },
+});
+const upload = multer({ storage });
 
 // router.post('/');
 
