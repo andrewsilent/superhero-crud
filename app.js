895551fdd9d5ca0fd762df9ba2e08ffb40fde494
andrewsilent <silent.netix@gmail.com>
2021-04-12 14:17:16 +0300

    add error handler

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const router = require('./routes');
+const errorHandler = require('./middlewares/errorHandler.mw');
 
 const app = express();
 
 app.use(express.json());
 app.use('/api', router);
 
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  res.status(status).send({
-    errors: [{ message: err.message || 'Serber error ' }],
-  });
-});
+app.use(errorHandler);
 
 module.exports = app;
