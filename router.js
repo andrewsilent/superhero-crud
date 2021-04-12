584e5d6db7a2eebafd25c7db8526ef529b1c8839
andrewsilent <silent.netix@gmail.com>
2021-04-12 12:38:44 +0300

    update routes

--- router.js
+++ router.js
@@ -1,25 +1,28 @@
 const { Router } = require('express');
-const multer = require('multer');
+const { imageUpload } = require('./middlewares/imageUpload.mw');
+const { heroInstance } = require('./middlewares/heroInstance.mw');
+const { powersAdd } = require('./middlewares/powersAdd.mw');
 const SuperheroController = require('./controllers/superhero.controller');
+const { imageAdd } = require('./middlewares/imageAdd.mw');
 const router = Router();
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './public/images');
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}.${file.originalname}`);
-  },
-});
-const upload = multer({ storage });
 
 router.post(
   '/superheroes',
-  /*upload.array('image')*/ SuperheroController.createHero,
+  imageUpload,
+  imageAdd,
+  powersAdd,
+  SuperheroController.createHero,
 );
-router.get('/superheroes');
-router.get('/superheroes/:id');
-router.patch('/superheroes/:id');
+router.get('/superheroes', SuperheroController.getAllHeroes);
+router.get('/superheroes/:id', SuperheroController.getHero);
+router.patch(
+  '/superheroes/:id',
+  heroInstance,
+  imageUpload,
+  imageAdd,
+  powersAdd,
+  SuperheroController.updateHero,
+);
 router.delete('/superheroes/:id', SuperheroController.deleteHero);
 
 module.exports = router;
